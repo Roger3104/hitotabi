2f2dcc0ea8baa1182ec01f01d773f37eb99f0762
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-03-07 06:29:55 +0000

    ADD routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,41 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+
+  scope module: :public do
+    root to: "homes#top"
+    get "/about"=>"homes#about"
+    resources :posts do
+      resource :favorites, only: [:index, :create, :destroy]
+      resources :comments, only: [:create, :destroy]
+    end
+    resource :users, only: [:index, :show, :edit, :update] do
+      get :check, on: :collection
+      patch :withdrawal, on: :collection
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+    resources :tags, only: [:index]
+  end
+
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :posts, only: [:index, :show, :destroy]
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :tags, except: [:destroy]
+    resources :orders, only: [:show, :update]
+    resources :order_products, only: [:update]
+  end
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
