f8480441043a1896f3003be58415492c3d735050
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-03-07 14:28:18 +0000

    start category

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,11 @@ Rails.application.routes.draw do
 
   # 顧客用
   # URL /customers/sign_in ...
-  devise_for :users, controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
+  devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
   }
+
 
   namespace :admin do
     root to: "homes#top"
@@ -38,8 +39,8 @@ Rails.application.routes.draw do
   end
   # 管理者用
   # URL /admin/sign_in ...
-  devise_for :admin, controllers: {
-    sessions: "admin/sessions"
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
