af38d394c90df1f593ed36185b87348102b6b15d
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-03-13 08:29:15 +0000

    add tag show page

--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -2,6 +2,6 @@ class Public::TagsController < ApplicationController
   def show
     @tags = Tag.all
     @tag = Tag.find(params[:id])
-    @tag_post = @tag.posts.all.order("created_at DESC").page(params[:page])
+    @tag_posts = @tag.posts.all.order("created_at DESC").page(params[:page])
   end
 end
