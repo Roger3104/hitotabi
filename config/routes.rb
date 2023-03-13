fed3df77ec0f3025f5661e7704a2c687b692b403
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-03-13 08:12:39 +0000

    fixed favorite feature but still need to fix Ajax

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     root to: "homes#top"
     get "/about"=>"homes#about"
     resources :posts do
-      resources :favorites, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
       resources :comments, only: [:create, :destroy]
       collection do
         get 'confirm'
