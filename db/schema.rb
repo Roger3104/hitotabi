f6275aea55c2bc167f3b8ee480f675ee533fad9c
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-04-20 13:58:15 +0000

    edit view page for report feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_26_064249) do
+ActiveRecord::Schema.define(version: 2023_04_17_144754) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -110,6 +110,16 @@ ActiveRecord::Schema.define(version: 2023_03_26_064249) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reports", force: :cascade do |t|
+    t.integer "reporter_id", null: false
+    t.integer "reported_id", null: false
+    t.text "reason", null: false
+    t.text "url"
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
     t.integer "category_id", null: false
@@ -139,6 +149,7 @@ ActiveRecord::Schema.define(version: 2023_03_26_064249) do
     t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "user_status", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
