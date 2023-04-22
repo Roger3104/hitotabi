0317da31fe99be3480eccbb575fd5ae2acc29327
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-04-22 05:16:56 +0000

    edited report button design

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -12,7 +12,7 @@ class Admin::ReportsController < ApplicationController
     @report = Report.find(params[:id])
     if @report.update(report_params)
       flash[:notice] = "対応ステータスを更新しました。"
-　　　redirect_to request.referer
+      redirect_to request.referer
     end
   end
 
