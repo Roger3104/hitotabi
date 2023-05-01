c5a9350c39e8e7997bb40c5c241f0ca0e290cfed
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-05-01 12:36:28 +0000

    edited report controller but error

--- app/controllers/public/reports_controller.rb
+++ app/controllers/public/reports_controller.rb
@@ -1,26 +1,26 @@
 class Public::ReportsController < ApplicationController
-  
+
   def new
     @report = Report.new
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
   end
-  
+
   def create
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
     @report = Report.new(report_params)
     @report.reporter_id = current_user.id
     @report.reported_id = @user.id
-    if @report.save 
+    if @report.save
       redirect_to user_path(@user), notice: "ご報告ありがとうございます"
     else
       render "new"
     end
   end
-  
-  
-  
+
+
+
   private
-  
+
   def report_params
     params.require(:report).permit(:reason, :url)
   end
